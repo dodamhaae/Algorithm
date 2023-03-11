3779531fa5ee420a6a3c09ff279657fa448432be
dodamhaae <djlim125@gmail.com>
2023-03-11 22:48:38 +0900

    중복된 숫자 개수: count()

--- Level-0/중복된-숫자-개수.py
+++ Level-0/중복된-숫자-개수.py
@@ -1,6 +1,2 @@
-def solution(array, n):
-    answer = 0
-    for i in array:
-        if i == n:
-            answer+=1        
-    return answer
+def solution(array, n): 
+    return array.count(n)
